d1acd99eba2d167dea6d12e181510018ebdc416f
Loventia <48926260+Loventia@users.noreply.github.com>
2023-10-23 19:30:25 +0200

    added supabase providers

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+
+//import {useEffect, useState } from "react";
+
+import React from 'react'
+
+const ModalProvider = () => {
+    const [isMoutend, setIsMounted]= useState(false);
+    useEffect(()=>{
+        setIsMounted(true)
+        },[]);
+    if(!isMoutend){
+        return null;
+    }
+  return (
+    <div>Modals!</div>
+  )
+}
+
+export default ModalProvider;
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -6,6 +6,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import {Database} from "@/types_db";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
+
 interface SupabaseProviderProps{
     children: React.ReactNode;
 };
@@ -14,7 +15,12 @@ const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
     children
   }) => {
       const [supabaseClient] = useState(() =>
-      createClientComponentClient<Database>()
+      createClientComponentClient<Database>({
+        supabaseUrl: "https://pjpyniqseatntkpctkcp.supabase.co",
+        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqcHluaXFzZWF0bnRrcGN0a2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyMDc4MDcsImV4cCI6MjAxMjc4MzgwN30.296qGu73XzFGncwMofhvod5ngRh8v9YKSJ5inrRwajFI",
+      }
+      )
+      
     );
   
     return ( 
